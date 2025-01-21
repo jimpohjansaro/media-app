2ceb7667664e7172390794ed26e0fd6895d38808
Jim Pohjansaro <jimp@metropolia.fi>
2025-01-21 13:56:39 +0200

    first message

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Home from "./components/Home";
+const App = () => {
+  return (
+    <>
+      <h1>My App</h1>
+      <Home></Home>
+    </>
+  );
+};
+export default App;
